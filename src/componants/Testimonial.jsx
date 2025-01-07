f451a8b323b9d0e7efeb22b7b0eba6556cc88a33
sujon-chambugong <sujonchambugong@gmail.com>
2025-01-07 16:22:17 +0600

    change vite buld to npm run build

--- src/componants/Testimonial.jsx
+++ src/componants/Testimonial.jsx
@@ -1,31 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const Testimonial = () => {
-  const testimonials = [
-    {
-      id: 1,
-      text: "This app has made managing my loans and savings so much easier. Highly recommended!",
-      name: "John Doe",
-      role: "Small Business Owner",
-      image: "https://via.placeholder.com/50",
-    },
-    {
-      id: 2,
-      text: "A must-have tool for anyone looking to simplify their finances. The interface is fantastic!",
-      name: "Jane Smith",
-      role: "Freelancer",
-      image: "https://via.placeholder.com/50",
-    },
-    {
-      id: 3,
-      text: "Managing expenses has never been easier. The support team is also amazing!",
-      name: "Michael Brown",
-      role: "Entrepreneur",
-      image: "https://via.placeholder.com/50",
-    },
-  ];
-
- 
+  const [testimonials, setTestimonials] = useState([]);
+  console.log(testimonials);
+  useEffect(() => { 
+    const fetchTestimonials = async () => {
+      try {
+        const response = await axios.get("https://amar-savings-loan.onrender.com/api/testimonials/getTestimonials");
+        setTestimonials(response.data);
+      } catch (error) {
+        console.error("There was an error!", error);
+      }
+    };
+    fetchTestimonials();
+  }, []);
 
   return (
     <section className="bg-white py-16">
@@ -33,13 +23,13 @@ const Testimonial = () => {
                     <h2 className="text-2xl font-bold text-center mb-8">What Our Users Say</h2>
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     {/* <!-- Testimonial 1 --> */}
-                    {testimonials.map(testi=><div key={testi.id} className="bg-gray-50 p-6 rounded shadow">
+                    {testimonials.map(testi=><div key={testi._id} className="bg-gray-50 p-6 rounded shadow">
                         <p className="text-gray-600 italic">
                         &quot; { testi.text} &quot;
                         </p>
                         <div className="mt-4 flex items-center">
                         <img 
-                            src="https://via.placeholder.com/50" 
+                            src={testi.image} 
                             alt="User 1" 
                             className="w-12 h-12 rounded-full mr-3"
                         />
